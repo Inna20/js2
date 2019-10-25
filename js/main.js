3c9292fe23043a6ace34022ebac2a67dbeb421f1
Inna <i.yemelyanova@gmail.com>
2019-10-25 22:56:58 +0300

    DZ

--- js/main.js
+++ js/main.js
@@ -5,16 +5,17 @@ const products = [
   {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price) => {
-  return `<div class="product-item">
-            <h3>${title}</h3>
-            <p>${price}</p>
-          </div>`;
-};
+const renderProduct = (title, price, img='http://placehold.it/150x150') => `<div class="product-item">
+            <img src="${img}"/>
+            <div>
+              <h3>${title}</h3>
+              <p>${price}$</p> 
+             </div>
+          </div>`;  // Задание 2
 
 const renderProducts = goodsList => {
   const productsList = goodsList.map(good => renderProduct(good.title, good.price));
-  document.querySelector('.products').innerHTML = productsList;
+  document.querySelector('.products').innerHTML = productsList.join(''); // Задание 3
 };
 
 renderProducts(products);
