3d511217673822a595538d476d1ad3cb61371610
Emelyanova Inna <iemelyanova@hsmedia.ru>
2019-11-12 15:11:11 +0300

    lessson5 start

--- js/main.js
+++ js/main.js
@@ -1,127 +1,30 @@
 const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
-// Переделать в ДЗ
-let getRequest_old = (url, cb) => {
-  let xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4) {
-      if (xhr.status !== 200) {
-        console.log('Error');
-      } else {
-        cb(xhr.responseText);
-      }
-    }
-  };
-  xhr.send();
-};
-
-// with Promise
-let getRequest = (url) => {
-  return new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status !== 200) {
-          reject('Error');
-        } else {
-          resolve(xhr.responseText);
-        }
-      }
-    };
-    xhr.send();
-  })
-  }
-
-class ProductItem {
-  constructor(product, img = 'https://placehold.it/200x150') {
-    this.title = product.product_name;
-    this.price = product.price;
-    this.id = product.id;
-    this.img = img;
-  }
-
-  render() {
-    return `<div class="product-item" data-id="${this.id}">
-                <img src="${this.img}" alt="Some img">
-                <div class="desc">
-                    <h3>${this.title}</h3>
-                    <p>${this.price} \u20bd</p>
-                    <button class="buy-btn">Купить</button>
-                </div>
-            </div>`;
-  }
-}
-
-class ProductList {
-  constructor(container = '.products') {
-    this.container = container;
-    this.goods = [];
-    this.allProducts = [];
-    // this._getProducts()
-    //   .then(data => {
-    //     this.goods = [...data];
-    //     this._render();
-    //   });
-    this._fetchProducts();
-    // this._render();
-  }
-
-  _fetchProducts() {
-    // getRequest(`${API}/catalogData.json`, (data) => {
-    //   this.goods = JSON.parse(data);
-    //   this._render();
-    //   console.log(this.goods);
-    // });
-    return getRequest(`${API}/catalogData.json`)
-        .then(result => {
-          this.goods = JSON.parse(result);
-          this._render();
-        })
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: '/catalogData.json',
+    products: [],
+    imgCatalog: 'https://placehold.it/200x150'
+  },
+  methods: {
+    getJson(url){
+      return fetch(url)
+        .then(result => result.json())
         .catch(error => {
-          console.log('Error: ', error);
-        });
-  }
-
-  // _getProducts() {
-  //   return fetch(`${API}/catalogData.json`)
-  //     .then(result => result.json())
-  //     .catch(error => {
-  //       console.log('Error: ', error);
-  //     });
-  // }
-
-  _render() {
-    const block = document.querySelector(this.container);
-
-    for (let product of this.goods) {
-      const productObject = new ProductItem(product);
-      this.allProducts.push(productObject);
-      block.insertAdjacentHTML('beforeend', productObject.render());
+          console.log(error);
+        })
+    },
+    addProduct(product){
+      console.log(product.id_product);
     }
+  },
+  mounted(){
+    this.getJson(`${API + this.catalogUrl}`)
+      .then(data => {
+        for(let el of data){
+          this.products.push(el);
+        }
+      });
   }
-}
-
-const list = new ProductList();
-
-
-
-// const products = [
-//   {id: 1, title: 'Notebook', price: 40000},
-//   {id: 2, title: 'Mouse', price: 1000},
-//   {id: 3, title: 'Keyboard', price: 2500},
-//   {id: 4, title: 'Gamepad', price: 1500},
-// ];
-//
-// const renderProduct = (item, img = 'https://placehold.it/200x150') => `<div class="product-item">
-//             <img src="${img}" alt="Some img">
-//             <h3>${item.title}</h3>
-//             <p>${item.price}</p>
-//             <button class="by-btn">Добавить</button>
-//           </div>`;
-//
-// const renderProducts = list => document.querySelector('.products')
-//   .insertAdjacentHTML('beforeend', list.map(item => renderProduct(item)).join(''));
-//
-// renderProducts(products);
+});
